28216107b7dcd3d426e082159bd6f4e562034053
agarwalpiyush87 <54887941+agarwalpiyush87@users.noreply.github.com>
2019-09-04 12:03:27 +0300

    Update 3GPP_NR_5G_PUSCH_Resource_Allocation_TimeDomain.py
    Some correction to get this code working. Thanks for the headstart thoo. :)

--- 3GPP_NR_5G_PUSCH_Resource_Allocation_TimeDomain.py
+++ 3GPP_NR_5G_PUSCH_Resource_Allocation_TimeDomain.py
@@ -1,6 +1,6 @@
 import math																# for floor function
 import string															# for replace function
-print "\nThis utility is for PUSCH Resource Allocation in Time domain only\nPlease refer to 38.214 Section5.1.2.1\n"
+print ("\nThis utility is for PUSCH Resource Allocation in Time domain only\nPlease refer to 38.214 Section5.1.2.1\n")
 # Time_Domain_Resource_Index = int(raw_input("Time_Domain_Resource_Index in DCI(3bits)> ")) 
 # PDSCH_Mapping_Type = raw_input("A or B > ").upper()
 
@@ -9,17 +9,19 @@ print "\nThis utility is for PUSCH Resource Allocation in Time domain only\nPlea
 # else:
 	# print "Valid start(S) value {0,1,2,3,4,5,6,7,8,9,10,11,12} for SLIV\nValid Length(L) value {2,4,7}\n"
 
-Start_Symbol = int(raw_input("Please enter the Start (S) Value> "))
-Length_Value = int(raw_input("Please enter the Length (L) Value> "))
+Start_Symbol = input("Please enter the Start (S) Value> ")
+Length_Value = input("Please enter the Length (L) Value> ")
+Start_Symbol = int(Start_Symbol)
+Length_Value = int (Length_Value)
 if Length_Value <= 14-Start_Symbol:
 	if Length_Value-1 <=7:
 		SLIV = 14*(Length_Value-1)+Start_Symbol
-		print "\nThe SLIV is %r" %SLIV
-		print "\nSLIV in CRM command %r" %hex(SLIV)
+		print ("\nThe SLIV is %r" %SLIV)
+		print ("\nSLIV in CRM command %r" %hex(SLIV))
 	else:
 		SLIV = 14*(14-Length_Value+1)+(14-1-Start_Symbol)
-		print "\nThe SLIV is %r" %SLIV
+		print ("\nThe SLIV is %r" %SLIV)
 		# print "\nSLIV in CRM command %r" %hex(SLIV)
 else:
-	print "The Value of L does not satisfy '0<L<=14-S'\nPlease refer to 38.214 Section 5.1.2.1"
+	print ("The Value of L does not satisfy '0<L<=14-S'\nPlease refer to 38.214 Section 5.1.2.1")
 # raw_input()
